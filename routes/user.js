d3118136b4c5ab6f4db83474ff3dec26662c7ee9
Omar Sobh <om.sobh@gmail.com>
2016-11-02 13:21:23 -0700

    latest features for launching containers

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,28 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
-
-
+var Docker = require('dockerode');
+var docker = new Docker({socketPath: '/var/run/docker.sock'});
+var docker2 = new Docker({host: 'http://dsp01.dyndns.org/docker'});
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
-  res.render('user', { user: req.user });
+  res.render('user', { user: req.user});
+});
+
+router.get('/datascience', ensureLoggedIn, function(req, res, next) {
+  res.render('datas', { user: req.user, title: "Data Science"});
+});
+
+router.get('/spark', ensureLoggedIn, function(req, res, next) {
+  res.render('spark', { user: req.user, title: "Spark"});
+});
+
+router.get('/tensorflow', ensureLoggedIn, function(req, res, next) {
+  res.render('tensorflow', { user: req.user, title: "Tensor Flow"});
+});
+
+router.get('/zepp', ensureLoggedIn, function(req, res, next) {
+  res.render('zepp', { user: req.user, title: "Zeppelin"});
 });
 
 module.exports = router;
